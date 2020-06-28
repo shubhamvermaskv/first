92dc37d248adc4f5203b79586ed7c8bcaec0598b
raman <kukreti.rs@gmail.com>
2020-06-28 16:24:12 +0530

    add port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8000;
+const PORT = process.env.port || 3000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
